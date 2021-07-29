d993693d6302a1ab92f4c9ef3ccd0898c1f7cf2a
Ghassan Achi <ghassan.achi@gmail.com>
2021-07-29 23:21:18 +0300

    update PATH and variables

--- fish/config.fish
+++ fish/config.fish
@@ -29,11 +29,13 @@ abbr -a gah 'git stash; and git pull --rebase; and git stash pop'
 
 # --- Path Overide ---------------------------------------------------
 
-set -x PATH /opt/local/bin /opt/lcal/sbin $PATH
+set -x PATH $PATH /opt/local/bin /opt/local/sbin 
 # Personal bin
 set -x PATH $PATH $HOME/.bin
+
 #RUST
-set -x PATH $HOME/.cargo/bin $PATH
+set -x PATH $PATH $HOME/.cargo/bin 
+set -x PATH $PATH $HOME/.cargo-target/release/
 
 # Android
 set -x PATH $PATH $ANDROID_HOME/emulator
